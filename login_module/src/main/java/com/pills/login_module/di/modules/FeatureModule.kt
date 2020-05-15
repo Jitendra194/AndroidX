faf0bf76699d0281b9a9010282508f2a68b29b12
Jitendra Reddy Syamala <jithendra194reddy@gmail.com>
2020-05-15 14:36:24 -0400

    WIP-1

--- login_module/src/main/java/com/pills/login_module/di/modules/FeatureModule.kt
+++ login_module/src/main/java/com/pills/login_module/di/modules/FeatureModule.kt
@@ -1,8 +1,9 @@
 package com.pills.login_module.di.modules
 
 import com.pills.login_module.di.components.CreateAccountComponent
+import com.pills.login_module.di.components.GoogleUserDetailsComponent
 import com.pills.login_module.di.components.LoginComponent
 import dagger.Module
 
-@Module(subcomponents = [LoginComponent::class, CreateAccountComponent::class])
+@Module(subcomponents = [LoginComponent::class, CreateAccountComponent::class, GoogleUserDetailsComponent::class])
 object FeatureModule
