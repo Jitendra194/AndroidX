ea49cbf484fe12941ead037fe129d44e3bcdc81b
Jitendra Reddy Syamala <jithendra194reddy@gmail.com>
2020-06-20 15:50:03 -0400

    WIP - 1
    Added retrofit dependencies.

--- app/src/main/java/com/pills/mydemoapplication/utils/animation_utils/ContainerTransformUtils.kt
+++ app/src/main/java/com/pills/mydemoapplication/utils/animation_utils/ContainerTransformUtils.kt
@@ -1,20 +1,17 @@
 package com.pills.mydemoapplication.utils.animation_utils
 
-import com.google.android.material.transition.MaterialArcMotion
 import com.google.android.material.transition.MaterialContainerTransform
 import com.pills.mydemoapplication.R
 
 fun MaterialContainerTransform.getEnterAnimation(): MaterialContainerTransform {
-    fadeMode = MaterialContainerTransform.FADE_MODE_CROSS
+    fadeMode = MaterialContainerTransform.FADE_MODE_IN
     drawingViewId = R.id.main_hq_navigation
     duration = 350.toLong()
-    setPathMotion(MaterialArcMotion())
     return this
 }
 
 fun MaterialContainerTransform.getExitAnimation() = this.apply {
     fadeMode = MaterialContainerTransform.FADE_MODE_OUT
     drawingViewId = R.id.main_hq_navigation
-    setPathMotion(MaterialArcMotion())
     duration = 350.toLong()
 }
